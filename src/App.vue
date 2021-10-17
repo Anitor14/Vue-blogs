a0b8db50bda500cbe37d17f728d77a313c918e09
Anitor <anitorabraham@gmail.com>
2021-10-18 00:02:49 +0100

    footer completed

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,18 @@
       <Navigation />
       <router-view />
     </div>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Navigation from "./components/Navigation.vue";
+import Footer from "./components/Footer.vue";
 export default {
   name: "app",
   components: {
     Navigation,
+    Footer,
   },
   data() {
     return {};
